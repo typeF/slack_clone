d6ce64857ec37f00465f731d4ace6c352623b5de
Frank Lam <frank.is.missing@gmail.com>
2018-03-10 17:09:05 -0800

    Added styling to team sidebars

--- slack_client/src/components/Teams.js
+++ slack_client/src/components/Teams.js
@@ -8,15 +8,61 @@ const TeamWrapper = styled.div`
   color: #958993;
 `;
 
+const TeamList = styled.ul`
+  width: 100%;
+  padding-left: 0px;
+  list-style: none;
+`;
+
+const TeamListItem = styled.li`
+  align-items: center;
+  background-color: #676066;
+  border-radius: 11px;
+  color: #fff;
+  display: flex;
+  font-size: 24px;
+  height: 50px;
+  justify-content: center;
+  margin: auto;
+  margin-bottom: 10px;
+  width: 50px;
+  &:hover {
+    border-style: solid;
+    border-width: thick;
+    border-color: #767676;
+  }
+`;
+
+// const TeamListItem = styled.li`
+//   height: 50px;
+//   width: 50px;
+//   background-color: #676066;
+//   color: #fff;
+//   margin: auto;
+//   margin-bottom: 10px;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   font-size: 24px;
+//   border-radius: 11px;
+//   &:hover {
+//     border-style: solid;
+//     border-width: thick;
+//     border-color: #767676;
+//   }
+// `;
+
 const team = ({id, name}) => (
-  <li key={`team-${id}`}>{name}</li>
+  <TeamListItem key={`team-${id}`}>
+    {name}
+  </TeamListItem>
 );
 
 export default ({teams}) => (
   <TeamWrapper>
-    <ul>
+    <TeamList>
       {teams.map(team)}
-    </ul>
+    </TeamList>
   </TeamWrapper>
 );
 
